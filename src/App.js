5484d5731e816f8cb527185a01db29e7554ee34a
Adam M Deeley <ad.deeley@gmail.com>
2018-03-08 20:26:42 +0000

    First working version.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+const m = require('marked');
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     //this.content = m('# Marked in browser\n\nRendered by **marked**.')
-    this.state = {content: ''};
+    this.state = {__html: ''};
     this.translateMarkdown = this.translateMarkdown.bind(this);
   }
 
   translateMarkdown(event) {
-    this.setState({content: event.target.value})
+    this.setState({__html: m(event.target.value)})
   }
 
   render() {
     return (
       <div className="container">
-        {this.content}
         <textarea 
           className="input-text-box"
           onChange={this.translateMarkdown}
           >
         </textarea>
-        <textarea 
-          className="input-text-box"
-          readOnly="true"
-          value={this.state.content}>
-        </textarea>
+        <div 
+          className="output-text-box"
+          dangerouslySetInnerHTML={this.state} />
       </div>
     );
   }
